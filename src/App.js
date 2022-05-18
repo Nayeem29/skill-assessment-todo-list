e59a5eb67ce70f71402786a8a2f0209883754bad
Nayeem29 <nayeembracu29@gmail.com>
2022-05-18 22:31:35 +0600

    firebase tailwind router page setup and navbar create

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import ToDo from './Pages/Home/ToDo';
+import Login from './Pages/Login/Login';
+import Register from './Pages/Login/Register';
+import Navbar from './Pages/SharedComponets/Navbar';
 
 function App() {
   return (
     <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<ToDo />}></Route>
+        <Route path='/home' element={<ToDo />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/signup' element={<Register />}></Route>
+      </Routes>
     </div>
   );
 }
