fd9ff59ccae9175d2af6bd29dd81891d09da6401
Nayeem29 <nayeembracu29@gmail.com>
2022-05-19 07:19:30 +0600

    update in private route

--- src/Pages/Login/RequireAuth.js
+++ src/Pages/Login/RequireAuth.js
@@ -7,12 +7,13 @@ import Loading from '../SharedComponets/Loading';
 const RequireAuth = ({ children }) => {
   const [user, loading] = useAuthState(auth);
   const loaction = useLocation();
-  if (!user) {
-    return <Navigate to="/login" state={{ from: loaction }} replace />;
-  }
   if (loading) {
     return <Loading />
   }
+  if (!user) {
+    return <Navigate to="/login" state={{ from: loaction }} replace />;
+  }
+
   return children;
 };
 
